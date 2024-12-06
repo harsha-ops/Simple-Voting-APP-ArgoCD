4e29ec1b61d0c186fd219a99ce5ba0897fc2b2d4
harsha <harsha.venkata220@gmail.com>
2024-12-06 22:44:25 +0000

    change in updatek8smanifest file and jenkinsfile of vote

--- updatek8smanifest.sh
+++ updatek8smanifest.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-REPO_URL="https://github.com/harsha-ops/Simple-Voting-APP-ArgoCD.git"
+REPO_URL="https://<your-username>:<your-token>@github.com/harsha-ops/Simple-Voting-APP-ArgoCD.git"
 
 git clone "$REPO_URL" /tmp/repo
 
@@ -10,9 +10,15 @@ cd /tmp/repo
 
 sed -i "s|image:.*|image: $2|g" k8s-manifest/$1-deploy.yml
 
+# Configure Git author details
+git config --global user.email "jenkins@example.com"
+git config --global user.name "Jenkins CI"
+
 git add .
 
 git commit -m "Update kubernetes Manifest"
+
+
 
 git push
 
